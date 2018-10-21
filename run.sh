3d2ac056bf6637d4e6f72ae18a47b9e286a4ab49
Erik <erik.helman@outlook.com>
2018-10-21 17:36:16 -0400

    Changes to be committed: 	modified:   run.sh
    Moved jMeter log inside results folder

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ mkdir $FOLDER
 pytest test_geo_functional.py --html=$FOLDER/geo_functional_`date +%Y%m%d%H%M%S`.html
 pytest test_geo_negative.py --html=$FOLDER/geo_negative_`date +%Y%m%d%H%M%S`.html
 
-jmeter -n -t geo_perf_test.jmx -l geo_perf_log_`date +%Y%m%d%H%M%S`.jtl -e -o $FOLDER/perf
+jmeter -n -t geo_perf_test.jmx -l $FOLDER/geo_perf_log_`date +%Y%m%d%H%M%S`.jtl -e -o $FOLDER/perf
 
